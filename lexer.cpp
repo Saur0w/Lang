d3818cc82e170ebb9a0039c03d4c633da1e51c1c
saur0w <sthapliyal085@gmail.com>
2025-12-15 11:35:52 +0530

    Lexer Design (incomplete)

--- lexer.cpp
+++ lexer.cpp
@@ -1,3 +1,7 @@
+//
+// Created by sthap on 15-12-2025.
+//
+
 #include "lexer.h"
 #include <cctype>
 #include <iostream>
@@ -17,6 +21,32 @@ char Lexer::advance() {
 Token Lexer::nextToken() {
     char current = peek();
     while (current == ' ' || current == '\t' || current == '\n' || current == '\r') {
-
+        if (current == '\n') line++;
+        advance();
+        current = peek();
     }
-}
+
+    if (current == '\0') return {TOKEN_EOF, "", line};
+
+    if (current == '{') {
+       advance();
+        return {TOKEN_LBRACE, "{", line};
+    }
+    if (current == '}') { advance(); return {TOKEN_RBRACE, "}", line}; }
+    if (current == '(') { advance(); return {TOKEN_LPAREN, "(", line}; }
+    if (current == ')') { advance(); return {TOKEN_RPAREN, ")", line}; }
+    if (current == ':') { advance(); return {TOKEN_COLON, ":", line}; }
+    if (current == ';') { advance(); return {TOKEN_SEMICOLON, ";", line}; }
+    if (current == '=') { advance(); return {TOKEN_ASSIGN, "=", line}; }
+    if (current == '+') { advance(); return {TOKEN_PLUS, "+", line}; }
+    if (current == '.') { advance(); return {TOKEN_DOT, ".", line}; }
+
+    if (isalpha(current)) {
+        std::string text;
+        while (isalnum(peek()) || peek() == '_') {
+            text += advance();
+        }
+        if (text == "class") return {TOKEN_CLASS, text, line};
+        if (text == "public") return {TOKEN_PUBLIC, text, line};
+        if (text == "int") return {TOKEN_INT, text, line};
+    }
